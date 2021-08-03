2335bfdd43de2105b904c4d2f53686f1d8425b88
Siddhartha Lahiri <slahiri@users.noreply.github.com>
2021-08-03 09:51:14 +0530

    completed unit testing

--- deck.go
+++ deck.go
@@ -14,7 +14,7 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 
-	cardSuits := []string{"Spade", "Diamonds", "Hearts", "Clubs"}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "King", "Queen", "Jack"}
 
 	for _, suit := range cardSuits {
--- deck_test.go
+++ deck_test.go
@@ -1,10 +1,32 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 	if len(d) != 52 {
 		t.Errorf("Expected deck length as 52 but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card as Ace of Spades but got %v", d[0])
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting.txt")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting.txt")
+
+	loadedDeck := newDeckFromFile("_decktesting.txt")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length from file as 52 but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting.txt")
 }
